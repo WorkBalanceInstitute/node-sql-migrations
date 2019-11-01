d65b18ad769e604cb435832990feb23b393974fa
Jakob Buverud <jbuverud@gmail.com>
2019-11-01 11:27:30 +0100

    Update sql filename format

--- commands/run-migrations-command.js
+++ commands/run-migrations-command.js
@@ -34,7 +34,7 @@ function getPending(migrationsList, appliedMigrationIds, minMigrationTime) {
     var pending = [];
     migrationsList.forEach(function (migration) {
         var id = migration.match(/^(\d+)/)[0];
-        if ((!minMigrationTime || id >= minMigrationTime) && !~appliedMigrationIds.indexOf(id) && migration.match(/^\d+\_up.*$/)) {
+        if ((!minMigrationTime || id >= minMigrationTime) && !~appliedMigrationIds.indexOf(id) && migration.match(/^\d+\w+\.up.sql*$/)) {
             pending.push(migration);
         }
     });
